076f97bfcf00e0a20a3efda85f295fe7f74df2c0
Simon <simon.ts@rays.ai>
2023-02-06 16:57:17 -0800

    first version of useAutoResize - both horizontal directions

--- pages/experimental/auto-resize.js
+++ pages/experimental/auto-resize.js
@@ -1,6 +1,47 @@
 import Head from "next/head";
+import { useState, useRef } from "react";
 
 const AutoResize = () => {
+  const [toggle, targ] = useAutoResize("width");
+
+  function useAutoResize(orientation) {
+    const [isExpanded, setIsExpanded] = useState(true);
+    const targ = useRef();
+    const expandedW = useRef(0);
+    //console.log(targ);
+
+    function toggle() {
+      if (targ.current) {
+        targ.current.style.transition = "all 0.25s linear";
+        if (isExpanded) {
+          console.log(targ.current.offsetWidth);
+          expandedW.current = targ.current.offsetWidth;
+          targ.current.style.width = 0;
+          //targ.current.style.left = expandedW.current.toString() + "px"; //"200px";
+          setIsExpanded(false);
+        } else {
+          //console.log(expandedW.current);
+          targ.current.style.width = expandedW.current.toString() + "px";
+          //targ.current.style.left = "0px";
+          setIsExpanded(true);
+        }
+      }
+      return;
+    }
+
+    let o = orientation.toLowerCase();
+    if (o == "width" || o == "horizontal") {
+      return [toggle, targ];
+    }
+    if (o == "height" || o == "vertical") {
+      return [toggle, targ];
+    }
+
+    throw new Error(
+      "INVALID ORIENTATION ERROR: Orientation must be 'width', 'height', 'horizontal', or 'vertical'"
+    );
+  }
+
   return (
     <>
       <Head>
@@ -12,10 +53,18 @@ const AutoResize = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="min-h-[100vh] w-full bg-slate-900 text-slate-200 flex flex-row">
-        <div className="w-1/4 flex flex-col gap-4">
+      <div className="min-h-[100vh] w-full bg-slate-900 text-slate-200">
+        {" "}
+        {/*flex flex-row items-center">*/}
+        <div
+          id="TARGET"
+          ref={targ}
+          //style={{ transition: "width 0.25s linear" }}
+          className="absolute top-[100px] w-[200px] h-[50px] flex flex-col gap-4 border border-slate-100 text-center overflow-hidden"
+        >
           {"Example text to test resizing animation"}
         </div>
+        <button onClick={toggle}>{"Toggle"}</button>
       </div>
     </>
   );
