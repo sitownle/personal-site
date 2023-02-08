e5870f9a59b5c7953bb0be70a359a48d9c8e47d5
Simon <simon.ts@rays.ai>
2023-02-07 17:24:41 -0800

    added project navigation and finished basic useAutoResize functionality

--- pages/experimental/auto-resize.js
+++ pages/experimental/auto-resize.js
@@ -2,51 +2,90 @@ import Head from "next/head";
 import { useState, useRef } from "react";
 
 const AutoResize = () => {
-  const [toggle, targ] = useAutoResize("width");
+  const [toggle, targ] = useAutoResize({
+    orientation: "width",
+    direction: "left"
+  });
+  const [toggleRight, targRight] = useAutoResize({
+    orientation: "width",
+    direction: "right"
+  });
+  const [toggleDown, targDown] = useAutoResize({
+    orientation: "height",
+    direction: "down"
+  });
+  const [toggleUp, targUp] = useAutoResize({
+    orientation: "height",
+    direction: "up"
+  });
 
-  // TODO: add vertical direction, add config to root at opposite end, make param object so some can be omitted easily
-  function useAutoResize(orientation) {
+  // TODO: Smooth out repositioning, prevent spam halfsize error, test in other layouts
+  function useAutoResize(config) {
     const [isExpanded, setIsExpanded] = useState(true);
     const targ = useRef();
     const expandedW = useRef(0);
+    const origPos = useRef(0);
     //console.log(targ);
+    const dir = config.direction.toLowerCase();
 
     function toggle() {
       if (targ.current) {
         targ.current.style.transition = "all 0.25s linear";
         if (isExpanded) {
-          console.log(targ.current.offsetWidth);
           expandedW.current = targ.current.offsetWidth;
           targ.current.style.width = 0;
-          //targ.current.style.left = expandedW.current.toString() + "px"; //"200px";
+          if (dir == "right")
+            targ.current.style.left = expandedW.current.toString() + "px"; //"200px";
           setIsExpanded(false);
         } else {
-          //console.log(expandedW.current);
           targ.current.style.width = expandedW.current.toString() + "px";
-          //targ.current.style.left = "0px";
+          if (dir == "right") targ.current.style.left = "0px";
           setIsExpanded(true);
         }
       }
       return;
     }
 
-    let o = orientation.toLowerCase();
+    function toggleVert() {
+      if (targ.current) {
+        targ.current.style.transition = "all 0.25s linear";
+        if (isExpanded) {
+          expandedW.current = targ.current.offsetHeight;
+          targ.current.style.height = 0;
+          origPos.current = targ.current.style.top;
+          if (dir == "down") {
+            let newH = parseFloat(targ.current.offsetTop) + expandedW.current;
+            targ.current.style.top = newH.toString() + "px";
+          }
+          setIsExpanded(false);
+        } else {
+          targ.current.style.height = expandedW.current.toString() + "px";
+          if (dir == "down") {
+            targ.current.style.top = origPos.current;
+          }
+          setIsExpanded(true);
+        }
+      }
+      return;
+    }
+
+    let o = config.orientation.toLowerCase();
     if (o == "width" || o == "horizontal") {
       return [toggle, targ];
     }
     if (o == "height" || o == "vertical") {
-      return [toggle, targ];
+      return [toggleVert, targ];
     }
 
     throw new Error(
-      "INVALID ORIENTATION ERROR: Orientation must be 'width', 'height', 'horizontal', or 'vertical'"
+      "INVALID ORIENTATION ERROR: Orientation must be 'width', 'height', 'horizontal', or 'vertical', and direction must be 'up', 'down', 'left', or 'right'"
     );
   }
 
   return (
     <>
       <Head>
-        <title>{"auto-resize antimation hook"}</title>
+        <title>{"auto-resize animation hook"}</title>
         <meta
           name="description"
           content=" for eventual use in my projects/deployment to npm registry"
@@ -60,12 +99,32 @@ const AutoResize = () => {
         <div
           id="TARGET"
           ref={targ}
-          //style={{ transition: "width 0.25s linear" }}
           className="absolute top-[100px] w-[200px] h-[50px] flex flex-col gap-4 border border-slate-100 text-center overflow-hidden"
         >
-          {"Example text to test resizing animation"}
+          {"Resize me to the left!"}
         </div>
         <button onClick={toggle}>{"Toggle"}</button>
+        <div
+          ref={targRight}
+          className="absolute top-[200px] w-[200px] h-[50px] flex flex-col gap-4 border border-slate-100 text-center overflow-hidden"
+        >
+          {"Resize me to the right!"}
+        </div>
+        <button onClick={toggleRight}>{"Toggle Right!"}</button>
+        <div
+          ref={targUp}
+          className="absolute top-[300px] w-[200px] h-[50px] flex flex-col gap-4 border border-slate-100 text-center overflow-hidden"
+        >
+          {"Resize me to the top!"}
+        </div>
+        <button onClick={toggleUp}>{"Toggle Up!"}</button>
+        <div
+          ref={targDown}
+          className="absolute top-[400px] w-[200px] h-[50px] flex flex-col gap-4 border border-slate-100 text-center overflow-hidden"
+        >
+          {"Resize me to the bottom!"}
+        </div>
+        <button onClick={toggleDown}>{"Toggle Down"}</button>
       </div>
     </>
   );
---
+++ pages/experimental/index.js
@@ -0,0 +1,90 @@
+import Head from "next/head";
+import Link from "next/link";
+
+const projects = ["rays.ai", "auto-resize", "enchrome"];
+const links = { "rays.ai": "https://rays.ai" };
+const descriptions = {
+  "auto-resize":
+    "A small hook inspired by useAutoAnimate to simplify the animation I find myself reaching for often, but find more difficult to implement in my normal tailwind workflow",
+  enchrome:
+    "A password encryption chrome extension that provides an extra level of security for all levels of users",
+  "rays.ai":
+    "My first job! I built, deployed, and managed all parts of the website, the most significant of which was the graphical editor for optical design exploration with collaborative editing, unlimited undo/redo and a microservice written in golang for powering ray tracing computations"
+};
+
+const ProjectIndex = () => {
+  function changeCase(origStr) {
+    return origStr?.replace(/-/g, " ");
+  }
+
+  function Bridge({ project }) {
+    function getLink(project) {
+      console.log(project);
+      if (links[project])
+        return (
+          <a
+            href={links[project]}
+            className="group p-4 flex flex-col gap-4 rounded text-slate-400 hover:text-slate-200 hover:bg-slate-700 cursor-pointer transition-all border border-slate-900 hover:border-slate-500"
+          >
+            <div className="flex gap-[4px]">
+              <p>{project}</p>
+              <p className="group-hover:translate-x-[4px]">&#10132;</p>
+            </div>
+            <p className="text-sm pl-2">{descriptions[project]}</p>
+          </a>
+        );
+      return (
+        <Link
+          href={`/experimental/${project}`}
+          className="group p-4 flex flex-col gap-2 rounded text-slate-400 hover:text-slate-200 hover:bg-slate-700 cursor-pointer transition-all border border-slate-900 hover:border-slate-500"
+        >
+          <div className="flex gap-[4px]">
+            <p>{project}</p>
+            <p className="group-hover:translate-x-[4px]">&#10132;</p>
+          </div>
+          <p className="text-sm pl-2">{descriptions[project]}</p>
+        </Link>
+      );
+    }
+
+    return project ? getLink(project) : null;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{"Project Index"}</title>
+        <meta name="description" content="Thoughts by Simon Townley-Smith" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="min-h-[100vh] w-full bg-slate-900 text-slate-200 flex flex-row gap-4">
+        <div className="w-1/4 flex flex-col gap-4">
+          <Link
+            href="/"
+            className="group bg-slate-500 text-slate-100 hover:bg-slate-100 hover:text-slate-500 transition-colors w-[200px] text-center rounded-sm ml-4 mt-4 px-2 flex pl-16"
+          >
+            <p className="rotate-180 w-[10px] text-slate-500 group-hover:translate-x-[-4px]">
+              &#10132;
+            </p>
+            Home
+          </Link>
+          <div className="flex">
+            <span className="pl-4 w-[180px]">Explore My Projects</span>
+            <p className="rotate-45 w-[10px]">&#10132;</p>
+          </div>
+          {/* {projects.map((key, value) => {
+            return <Bridge project={key} key={value} />;
+          })} */}
+        </div>
+        <div className="w-1/2 flex flex-col gap-6 mt-24 px-2">
+          {projects.map((key, value) => {
+            return <Bridge project={key} key={value} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProjectIndex;
