6cac96bc8f9afaa625710eb330612c16cfd11e29
Simon <simon.ts@rays.ai>
2023-03-15 12:46:48 -0700

    accessibility/seo fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,31 +4,31 @@ import Header from "./header.jsx";
 export const metadata = {
   title: {
     default: "Simon Townley-Smith",
-    template: "%s | Simon Townley-Smith",
+    template: "%s | Simon Townley-Smith"
+  },
+  description: "Projects & Thoughts by Simon Townley-Smith",
+  keywords: ["Simon Townley-Smith", "sitownle", "Full Stack Engineer"],
+  authors: [{ name: "Simon Townley-Smith" }],
+  colorScheme: "dark",
+  creator: "Simon Townley-Smith",
+  publisher: "Simon Townley-Smith",
+  openGraph: {
+    title: "Simon Townley-Smith",
     description: "Projects & Thoughts by Simon Townley-Smith",
-    keywords: ["Simon Townley-Smith", "sitownle", "Full Stack Engineer"],
-    authors: [{ name: "Simon Townley-Smith" }],
-    colorScheme: "dark",
-    creator: "Simon Townley-Smith",
-    publisher: "Simon Townley-Smith",
-    openGraph: {
-      title: "Simon Townley-Smith",
-      description: "Projects & Thoughts by Simon Townley-Smith",
-      url: "https://sitownle.dev",
-      siteName: "sitownle.dev",
-      images: [
-        {
-          url: "https://nextjs.org/og.png",
-          width: 800,
-          height: 600
-        }
-      ],
-      locale: "en-US",
-      type: "website",
-      themeColor: "black"
-    }
-    //icon:
+    url: "https://sitownle.dev",
+    siteName: "sitownle.dev",
+    images: [
+      {
+        url: "https://nextjs.org/og.png",
+        width: 800,
+        height: 600
+      }
+    ],
+    locale: "en-US",
+    type: "website",
+    themeColor: "black"
   }
+  //icon:
 };
 
 export default function RootLayout({
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="en">
       <head />
       <body>
         {/* <Head>
